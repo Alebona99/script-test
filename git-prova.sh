a2bc638c4e005f007974bc224f3792d04c926ac8
Alessio Bonanno <alessio.bonanno@infocube.it>
2021-10-20 14:47:20 +0200

    subtree funzionante

--- git-prova.sh
+++ git-prova.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 git reset --hard
-git subtree pull --prefix subtreeDirectory https://github.com/Alebona99/test_repo.git master --squash
-#git subtree pull --prefix subtreeDirectory https://github.com/Alebona99/test_repo.git master --squash | git merge --no-verify
+git subtree pull --prefix subtreeDirectory https://github.com/Alebona99/test_repo.git master --squash | git merge --no-verify
