79d0608615eb06c070346aedfbcfd2fcac741b21
Alessio Bonanno <alessio.bonanno@infocube.it>
2021-10-22 15:50:00 +0200

    update script

--- git-prova.sh
+++ git-prova.sh
@@ -2,4 +2,4 @@
 git reset --hard
 git subtree pull --prefix subtreeDirectory https://github.com/Alebona99/test_repo.git master --squash | git merge --no-verify
 git commit -m 'Merge e pull subtree'
-git push origin master
+git push origin main
